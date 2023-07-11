4d998f1d1de8a6a41a8343ec75f52071bce127a9
rootchang <changtiangen@gmail.com>
2023-07-11 14:13:51 -0400

    stable version 1

--- 02.Run_PENCIL.py
+++ 02.Run_PENCIL.py
@@ -7,14 +7,14 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0,1' #select a gpu id, e.g. '0', '0,1', us
 import warnings
 warnings.filterwarnings('ignore') #to ignore warnings on the output.
 import time
-
+from scipy.sparse import issparse
 
 start_time  = time.time()
 h5ad_fn = sys.argv[1] # '../02.Input/GSE200996/seu_Tissue_Tcell.h5ad'
-phenotype = sys.argv[2] # 'RECIST_response' # 'Volumetric_response'
+phenotype = sys.argv[2] # 'RECIST_response' # 'Volumetric_response' # "Any_response"
 data_name = sys.argv[3] # 'GSE200996'
 mode = sys.argv[4] # 'multi-classification'  'regression'
-use_HVG = int(sys.argv[5]) # 1: use high-variable-genes with scaled data. 0: use all genes with log1p expression
+use_HVG = int(sys.argv[5]) # 1: use high-variable-genes with scaled data. 0: use all genes with log1p expression (?)
 interrupt = int(sys.argv[6]) # 1: Interrupt to check the raw phenotypic labels. 0: no interruption.
 
 
@@ -36,39 +36,69 @@ values_to_remove = ['not measurable']
 adata = adata[~adata.obs[phenotype].isin(values_to_remove)]#.copy()
 labels_raw = adata.obs[phenotype]
 print('Clean labels: ', set(labels_raw))
-labels_raw = pd.Categorical(labels_raw)
-if use_HVG:
-    data, labels = adata.X.copy(), labels_raw.codes
-else:
-    data = adata.raw.X.copy().toarray()
+if mode == 'multi-classification':
+    labels_raw = pd.Categorical(labels_raw)
+    class_names = list(labels_raw.categories)
+    #print('class_labels: ', class_names)
     labels = labels_raw.codes
-class_names = list(labels_raw.categories)
+else:
+    labels = np.array(labels_raw.values, dtype=float)
+    #print('class_labels: ', labels)
+if use_HVG:
+    if issparse(adata.X):
+        data = adata.X.copy().todense()
+    else:
+        data = adata.X.copy()
+else:
+    if issparse(adata.raw.X):
+        data = adata.raw.X.copy().todense()
+    else:
+        data = adata.raw.X.copy()
+class_weights = None # [2.0, 1.0] # None
 emd = adata.obsm['X_umap']
 print('data.shape: ', data.shape)
+print('class_labels: ', labels)
 print('labels.shape: ', labels.shape)
-print('class_names: ', class_names)
+print('class_weights: ', class_weights)
 print('emd.shape: ', emd.shape)
 
-
 print('************** Step 2: running PENCIL ...')
-# class_weights = [2.0, 1.0]
-pencil = Pencil(mode, select_genes=True, seed=1234, data_name=data_name, expr_id=phenotype, mlflow_record=False,
-                model_types=['linear', 'non-linear'])
-pred, confidence = pencil.fit_transform(
-    data, labels,
-    test=True,
-    # c=None, # default is None
-    shuffle_rate=1/4, # 1/3, # default is 1/3
-    lambda_L1=1e-5, # 1e-4, # default is 1e-5
-    lambda_L2=1e-3, # default is 1e-3
-    lr=0.01, # default is 0.01
-    # epochs=500, # default is 500
-    # pre_train_epochs=500, # default = 500 for classification
-    class_weights=None, # class_weights, # default is None
-    class_names=class_names, # default is None
-    emd=emd,
-    plot_show=True
-    )
+if mode == 'multi-classification':
+    pencil = Pencil(mode, select_genes=True, seed=1234, data_name=data_name, expr_id=phenotype, mlflow_record=True,
+                    model_types=['linear', 'non-linear'])
+    with mlflow.start_run():
+        pred, confidence = pencil.fit_transform(
+            data, labels,
+            test=True,
+            # c=None, # default is None
+            shuffle_rate=1/3, # 1/4, # default is 1/3
+            lambda_L1=1e-5, # 1e-5, # default is 1e-5
+            lambda_L2=1e-3, # 1e-3, default is 1e-3
+            lr=0.01, # default is 0.01
+            # epochs=500, # default is 500
+            # pre_train_epochs=500, # default = 500 for classification
+            class_weights=class_weights, # class_weights, # default is None
+            class_names=class_names, # default is None
+            emd=emd,
+            plot_show=True
+            )
+else:
+    pencil = Pencil(mode, select_genes=True, seed=1234, data_name=data_name, expr_id=phenotype, mlflow_record=True,
+                    dropouts=[0.0, 0.0])  # `select_genes` can also be set to False, if True, pencil will output a weight vector for all 10 PCs.
+    with mlflow.start_run():
+        pred, confidence = pencil.fit_transform(
+            data, labels,
+            test=True,
+            shuffle_rate=1 / 5,
+            lambda_L1=1e-5,
+            lambda_L2=0.0,
+            lr=0.01,
+            epochs=2000,
+            class_weights=None,
+            emd=emd,
+            plot_show=True
+        )
+
 w = pencil.gene_weights(plot=True)
 plt.close()
 # pred_new, confidence_new = pencil.transform(data_new) # apply trained PENCIL on new data
