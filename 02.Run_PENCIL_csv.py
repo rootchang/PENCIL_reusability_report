1a55d9b0af56266392f0012e322084f4c8f9080d
rootchang <changtiangen@gmail.com>
2023-07-19 21:30:06 -0400

    updated figure1

--- 02.Run_PENCIL_csv.py
+++ 02.Run_PENCIL_csv.py
@@ -23,13 +23,11 @@ data_name = sys.argv[4] # 'GSE200996' 'GSE120575_Tissue_CD8T'
 phenotype = sys.argv[5] # 'ResponseInfo'
 mode = sys.argv[6] # 'multi-classification'  'regression'
 class_weight = float(eval(sys.argv[7])) # class_weights = [1.0, class_weight]
-exp_newdata_fn = sys.argv[8] # new test data
+exp_newdata_fn_list = sys.argv[8].split(',') # new test datasets, separates by ','
 
 print('************** Step 0: loading data ...')
 exp_df=pd.read_csv(exp_fn, sep=',',index_col=0)
 data=exp_df.values.T
-exp_new_df=pd.read_csv(exp_newdata_fn, sep=',',index_col=0)
-data_new=exp_new_df.values.T
 anno_df = pd.read_csv(anno_fn, sep=',', index_col=0, dtype=str)
 labels_raw = anno_df.values.flatten()
 
@@ -93,10 +91,15 @@ w = pencil.gene_weights(plot=True)
 plt.close()
 
 print('************** Step 3: predicting on new data ...')
-pred_new, confidence_new = pencil.transform(data_new) # apply trained PENCIL on new data
-save_fn = './results/'+data_name+'/py/'+phenotype+'/predicted_labels_test.csv'
-df = pd.DataFrame({'predicted_label': pred_new, 'confidence': confidence_new})
-df.loc[df['confidence'] < 0, 'predicted_label'] = 'Rejected'
-df.to_csv(save_fn, index=False)
+for i in range(len(exp_newdata_fn_list)):
+    print('    Working on test set %d ...'%(i+1))
+    exp_newdata_fn = exp_newdata_fn_list[i]
+    exp_new_df=pd.read_csv(exp_newdata_fn, sep=',',index_col=0)
+    data_new=exp_new_df.values.T
+    pred_new, confidence_new = pencil.transform(data_new) # apply trained PENCIL on new data
+    save_fn = './results/'+data_name+'/py/'+phenotype+'/predicted_labels_test'+str(i+1)+'.csv'
+    df = pd.DataFrame({'predicted_label': pred_new, 'confidence': confidence_new})
+    df.loc[df['confidence'] < 0, 'predicted_label'] = 'Rejected'
+    df.to_csv(save_fn, index=False)
 
 print('All done! Time used: %.2f s'%(time.time() - start_time))
